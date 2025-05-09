201bf07fc472a627e08b849db35c3b247fc38b0a
supkadam <supriyakadam2495@gmail.com>
2025-05-09 22:38:43 +0200

    added signup page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App(){
     return(
@@ -10,6 +11,7 @@ function App(){
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                     <Route path="/login" element={<Login/>}/>
+                    <Route path="/signup" element={<Signup/>}/>
                 </Routes>
             </BrowserRouter>
         </>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,19 +1,23 @@
 import React from "react";
-import '../css/Login.css'
+import '../css/Signup.css'
 import { useNavigate, Link } from "react-router-dom";  
 
 function Home(){
     return(
         <>
             <div className="container">
-                <div className="login-main">
+                <div className="signup-main">
                         <img src="chatlogo.png" width='160' height='140'/>
-                        <h1>Login</h1>
+                        <h1>Sign up</h1>
+
+                        <input type="text" placeholder="Username" style={{height:'45px', width:'250px', fontSize: '20px', paddingLeft: '5px', borderRadius: '10px', border: '0.5px solid grey'}}/>
                         
                         <input type="email" placeholder="Email" style={{height:'45px', width:'250px', fontSize: '20px', paddingLeft: '5px', borderRadius: '10px', border: '0.5px solid grey'}}/>
+
                         <input type="password" placeholder="Password" style={{height:'45px', width:'250px', fontSize: '20px', paddingLeft: '5px', borderRadius: '10px', border: '0.5px solid grey'}}/>
-                        <button id="loginbtn">Login</button>
-                        <Link to='/signup' style={{textDecoration: 'none'}}>Not a registered user? Signup</Link>
+
+                        <button id="loginbtn">Signup</button>
+                        <Link to='/login' style={{textDecoration: 'none'}}>Already registered? Login</Link>
                 </div>
             </div>
         </>
