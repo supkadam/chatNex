fe4c10d9578e88563b5244cdc2a30018ff91976d
supkadam <supriyakadam2495@gmail.com>
2025-05-09 22:26:29 +0200

    added login page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import '../css/Home.css'
+import { useNavigate } from "react-router-dom";  
+
 
 function Home(){
+    const navigate = useNavigate();
     return(
         <>
             <div className="container">
                 <div className="main">
-                        <img src="chatlogo.png" width='160' height='140' style={{borderRadius:'5px'}}/>
+                        <img src="chatlogo.png" width='160' height='140' />
                         <h1 style={{textAlign: 'center'}}>Welcome to the World of ChatNex</h1>
                         
-                        <p style={{fontSize:'22px', textAlign: 'center'}}>Chat Smarter.Discover More...</p>
-                        <button id="btn" style={{backgroundColor: '#b0acf0'}}>Continue</button>
+                        <p style={{fontSize:'22px', textAlign: 'center'}}>Chat fearlessly...</p>
+                        <button id="btn" onClick={()=>{navigate('/login')}}>Continue</button>
+                        
                 </div>
             </div>
         </>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import '../css/Home.css'
+import '../css/Login.css'
+import { useNavigate, Link } from "react-router-dom";  
 
 function Home(){
     return(
         <>
             <div className="container">
-                <div className="main">
-                        <img src="logo.png" width='180'/>
-                        <h1 style={{fontSize:'40px'}}>Login</h1>
+                <div className="login-main">
+                        <img src="chatlogo.png" width='160' height='140'/>
+                        <h1>Login</h1>
                         
-                        <input type="email" placeholder="Enter your email" style={{height:'45px', width:'250px'}}/>
-                        <input type="password" placeholder="Enter your password" style={{height:'45px', width:'250px'}}/>
-                        <button>Login</button>
+                        <input type="email" placeholder="Email" style={{height:'45px', width:'250px', fontSize: '20px', paddingLeft: '5px', borderRadius: '10px', border: '0.5px solid grey'}}/>
+                        <input type="password" placeholder="Password" style={{height:'45px', width:'250px', fontSize: '20px', paddingLeft: '5px', borderRadius: '10px', border: '0.5px solid grey'}}/>
+                        <button id="loginbtn">Login</button>
+                        <Link to='/signup'>Not a registered user? Signup</Link>
                 </div>
             </div>
         </>
